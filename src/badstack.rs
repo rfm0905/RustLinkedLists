af935a6efab44c5b7eb8c9f578ce17cc5f42790c
raed <rfm0905@gmail.com>
2025-02-13 00:15:38 -0500

    push and pop functions

--- src/badstack.rs
+++ src/badstack.rs
@@ -1,7 +1,36 @@
 // a list is either empty or has an element followed by another list
 
+use std::mem;
+
 pub struct List { // only keep structure public / accessible
     head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List {
+            head: Link::Empty
+        }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let newnode = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, Link::Empty) // replace to avoid borrowing issues
+        });
+
+        self.head = Link::More(newnode);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
 }
 enum Link {
     Empty,
